3911b667b730d1b803a6e55e0112206102433c83
wanckl <m.wanckl@gmail.com>
2019-04-26 23:12:53 +0800

    The first version: two MPU6050 OK, HeatRis OK, waitting for AS5045

---
+++ HWLIB/LED/led.c
@@ -0,0 +1,42 @@
+#include "led.h" 
+
+
+//初始化PC6和PC7为输出口.并使能这两个口的时钟
+//LED IO初始化
+void LED_Init(void)
+{    	 
+	GPIO_InitTypeDef  GPIO_InitStructure;
+
+	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOC, ENABLE);//使能GPIOF时钟
+
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_6 | GPIO_Pin_7;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;		//普通输出模式
+	GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;		//推挽输出
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;	//100MHz
+	GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_UP;		//上拉
+	GPIO_Init(GPIOC, &GPIO_InitStructure);				//初始化
+	
+	GPIO_ResetBits(GPIOC,GPIO_Pin_6 | GPIO_Pin_7);
+
+}
+
+
+void Heat_Res_Init(void)
+{    	 
+	GPIO_InitTypeDef  GPIO_InitStructure;
+
+	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOC, ENABLE);//使能GPIOF时钟
+
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_8 | GPIO_Pin_9;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;		//普通输出模式
+	GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;		//推挽输出
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;	//100MHz
+	GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_UP;		//上拉
+	GPIO_Init(GPIOC, &GPIO_InitStructure);				//初始化
+	
+	GPIO_ResetBits(GPIOC,GPIO_Pin_8 | GPIO_Pin_9);
+
+}
+
+
+
---
+++ User/main.c
@@ -0,0 +1,112 @@
+#include "stm32f4xx.h"
+#include <stdio.h>
+#include "sys.h"
+#include "delay.h"
+#include "usart.h"
+#include "iic.h"
+#include "mpu6050.h"
+#include "inv_mpu.h"
+#include "inv_mpu_dmp_motion_driver.h"
+#include "led.h"
+
+void RCC_ClockConfig(void);
+
+int main(void)
+{
+	uint8_t num = 0;
+
+	float pitch, roll, yaw;			//欧拉角
+	short accx, accy, accz;			//加速度原始数据
+	short gyrox, gyroy, gyroz;		//角速度原始数据
+	float Temp;						//温度
+	
+	RCC_ClockConfig();	//set sysclk to 168 MHz
+	delay_init(168);
+	uart_init(115200);
+	LED_Init();
+	Heat_Res_Init();
+	IIC_Init();
+	
+	if ( MPU_Init(MPU_F_ADDR) )
+	{
+		LED0 = 1;
+		printf("MPU6050 on TopLayer not found\n\n");
+	}
+	if ( MPU_Init(MPU_B_ADDR) )
+	{
+		LED1 = 1;
+		printf("MPU6050 on BottomLayer not found\n\n");
+	}
+	
+	R0 = 1;
+	R1 = 1;
+	
+	while (1)
+	{
+//		while(mpu_dmp_init())
+//		{
+//			printf("Wanl_%d\t\tMPU6050 Error.\n", num);
+//		}
+//		
+//		if(mpu_dmp_get_data(&pitch, &roll, &yaw) == 0)
+//		{ 
+			Temp = MPU_Get_Temperature(MPU_B_ADDR);					//得到温度值
+			MPU_Get_Accelerometer(MPU_B_ADDR, &accx, &accy, &accz);	//得到加速度传感器数据
+			MPU_Get_Gyroscope(MPU_B_ADDR, &gyrox, &gyroy, &gyroz);	//得到陀螺仪数据
+//		}
+		
+		num ++;
+		printf("Wanl_%d\t%.1f\t%d\t%d\t%d\t%d\t%d\t%d\t%.2f\t%.2f\t%.2f\n", num, Temp, accx, accy, accz, gyrox, gyroy, gyroz, pitch, roll, yaw);
+		delay_ms(100);
+	}
+}
+
+// re-config sysclk to 168 by need
+void RCC_ClockConfig(void)
+{
+	RCC_DeInit();
+	RCC_HSEConfig( RCC_HSE_ON );
+
+	if(SUCCESS == RCC_WaitForHSEStartUp())    
+	{
+		
+		RCC_HCLKConfig  (RCC_SYSCLK_Div1); 
+		RCC_PCLK1Config (RCC_HCLK_Div4);      
+		RCC_PCLK2Config (RCC_HCLK_Div2);        
+		RCC_PLLConfig   (RCC_PLLSource_HSE, 6, 168, 2, 7);	//SYSClock = 12/6*168/2 = 168M
+
+		RCC_PLLCmd(ENABLE);
+
+		while(RCC_GetFlagStatus ( RCC_FLAG_PLLRDY) == RESET )
+		{
+		}
+
+		RCC_SYSCLKConfig  ( RCC_SYSCLKSource_PLLCLK );
+
+		while(RCC_GetSYSCLKSource() != 0x08)
+		{
+		}
+	}
+}
+
+
+#ifdef  USE_FULL_ASSERT
+
+void assert_failed(uint8_t* file, uint32_t line)
+{ 
+	/* User can add his own implementation to report the file name and line number,
+		ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+
+	/* Infinite loop */
+	while (1)
+	{
+	}
+}
+#endif
+
+/**
+  * @}
+  */
+
+
+/**************** (C) COPYRIGHT STMicroelectronics *****END OF FILE*****************/
